93cb55c1383ea12d68eb21aa6725814cc96c3aab
najmiddin <turgunpolatov3gmail.com>
2023-11-30 08:27:59 +0500

    cimmit

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,5 @@
 import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  return (
-    <>
-      {/* <TopicsList /> */}
-      <h1>Salom hammaga</h1>
-    </>
-  )
+  return <TopicsList />
 }
